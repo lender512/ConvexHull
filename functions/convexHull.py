cc9bf0db4c674e5f897eae234b9dae425b4a12f3
Lender512 <luis.berrospi@utec.edu.pe>
2021-09-30 21:05:07 -0500

    half way

--- functions/convexHull.py
+++ functions/convexHull.py
@@ -6,10 +6,9 @@ mid = (0, 0)
 
 def clockwise(a, b, c):
     result = (b[1]-a[1])*(c[0]-b[0]) - (c[1]-b[1])*(b[0]-a[0])
-
     if result == 0:
         return 0
-    if result > 0:
+    elif result > 0:
         return 1
     return -1
 
@@ -45,7 +44,7 @@ def bubbleSort(arr):
             if not compare(arr[j], arr[j + 1]) :
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-def convexHullBase(points):
+def convexHullBase1(points):
     lista = []
     
     for i in range(len(points)):
@@ -90,7 +89,52 @@ def convexHullBase(points):
         lista[i] = (lista[i][0]//n, lista[i][1]//n)
 
     return lista
-        
+
+
+def Left_index(points):
+     
+    '''
+    Finding the left most point
+    '''
+    minn = 0
+    for i in range(1,len(points)):
+        if points[i][0] < points[minn][0]:
+            minn = i
+        elif points[i][0] == points[minn][0]:
+            if points[i][1] > points[minn][1]:
+                minn = i
+    return minn
+
+def convexHullBase(points):
+
+    if len(points) < 3:
+        return points
+ 
+    # Find the leftmost point
+    l = Left_index(points)
+    
+    hull = []
+     
+    p = l
+    q = 0
+    while(True):
+         
+        # Add current point to result
+        hull.append(points[p])
+
+        q = (p + 1) % len(points)
+ 
+        for i in range(len(points)):
+            if(clockwise(points[p],points[q], points[i]) == 1):
+                q = i
+
+        p = q
+
+        if(p == l):
+            break
+    
+    return hull
+
 def convexHullMerge(PointListA, PointListB):
     
     rightMostPointA = 0
@@ -99,11 +143,17 @@ def convexHullMerge(PointListA, PointListB):
     for i in range(1, len(PointListA)):
         if PointListA[i][0]>PointListA[rightMostPointA][0]:
             rightMostPointA = i
+        # elif PointListA[i][0]==PointListA[rightMostPointA][0]:
+        #     if PointListA[i][1]<PointListA[rightMostPointA][1]:
+        #         rightMostPointA = i
 
 
     for i in range(1, len(PointListB)):
         if PointListB[i][0]<PointListB[leftMostPointB][0]:
             leftMostPointB = i
+        # elif PointListB[i][0]==PointListB[leftMostPointB][0]:
+        #     if PointListB[i][1]<PointListB[leftMostPointB][1]:
+        #         leftMostPointB = i
 
 
     done = False
@@ -111,8 +161,6 @@ def convexHullMerge(PointListA, PointListB):
     b1 = leftMostPointB
     
     while not done:
-        a2 = (a1 + 1)%(len(PointListA))
-        b2 = (len(PointListB)+b1-1)%(len(PointListB))
         
         done = True
         while clockwise(PointListB[b1], PointListA[a1], PointListA[(a1 + 1)%(len(PointListA))])>=0:
@@ -130,8 +178,6 @@ def convexHullMerge(PointListA, PointListB):
     b1 = leftMostPointB
     while not done:
 
-        a2 = (len(PointListA)+a1-1)%(len(PointListA))
-        b2 = (b1 + 1)%(len(PointListB))
         
         done = True
         while clockwise(PointListA[a1], PointListB[b1], PointListB[(b1 + 1)%(len(PointListB))])>=0:
@@ -148,8 +194,8 @@ def convexHullMerge(PointListA, PointListB):
 
     mergeList = []
     mergeList.append(PointListA[upperA])
-    print("upperA:", PointListA[upperA])
-    print("lowerA:", PointListB[lowerB])
+    # print("upperA:", PointListA[upperA])
+    # print("lowerA:", PointListB[lowerB])
     while upperA != lowerA:
         upperA = (upperA+1)%len(PointListA)
         mergeList.append(PointListA[upperA])
@@ -164,17 +210,15 @@ def convexHullMerge(PointListA, PointListB):
 
 def convexHull(pointList):
     if len(pointList) <= 5:
+        print("Huby:", convexHullBase(pointList))
+        print("Luis:", convexHullBase1(pointList))
         return convexHullBase(pointList)
     
     left = pointList[0:len(pointList)//2]
-    print(left)
     right = pointList[len(pointList)//2:]
-    print(right)
     
     leftHull = convexHull(left)
-    print(leftHull)
     rightHull = convexHull(right)    
-    print(rightHull)
 
 
     return convexHullMerge(leftHull, rightHull)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys, pygame, random
-from functions.convexHull import convexHull
+from functions.convexHull import *
 
 
 pygame.init()
@@ -25,17 +25,33 @@ def transformList(list):
         list2.append(transform(list[i]))
     return list2
 
+
 def init():
-    N = 100
+
+    # print(convexHullBase([(-125, 123), (-100,14), (-54, 45)]))
+    # print(convexHullBase1([(-125, 123), (-100,14), (-54, 45)]))
+
+    N = 1000
     points = []
+
+    pointsHuby = [(-810,130),(-810,92),(-809,-380),(-805,-385),(-554,-405),(608,-401),(749,-398),(774,-392),(810,-226),(809,202),(801,325),(783,352),(758,367),(668,394),(404,403),(-58,405),(-650,403),(-797,393),(-802,310)]
+    pointsLender = [(-58, 405), (-650, 403), (-797, 393), (-802, 310), (-810, 130), (-810, 92), (-809, -380), (-805, -385), (-554, -405), (24, -403), (480, -398), (520, -382), (540, -280), (540, -233), (540, -262), (546, -355), (560, -401), (608, -401), (749, -398), (774, -392), (810, -226), (809, 202), (801, 325), (783, 352), (758, 367), (668, 394), (404, 403)]
+
     zoomOut = 0.9
     for n in range(N):
         points.append((random.randint(-width//2*zoomOut, width//2*zoomOut),(random.randint(-height//2*zoomOut, height//2*zoomOut)))) 
 
     #points = [(-126, 296), (-638,4), (-786,-447), (202,-84), (230, -355), (350, -362)]
-    testPoints = points
+    testPoints = list(set(points))
+
     testPoints.sort(key=lambda x: x[0])
     
+    string = ""
+    for point in testPoints:
+        string += "{" + str(point[0]) + "," + str(point[1]) + "}, "
+
+    print("{",string,"}")
+
     result = convexHull(testPoints)
 
     print(result)
@@ -48,14 +64,24 @@ def init():
         screen.fill(white)
 
         
+        
 
         for point in testPoints:
             pygame.draw.circle(screen, (100, 100, 100), transform(point), 5, 5)
 
         for point in result:
-            pygame.draw.circle(screen, (0, 0, 0), transform(point), 5, 2)
+            pygame.draw.circle(screen, (255, 0, 0), transform(point), 10, 2)
+
+
+        # for point in pointsHuby:
+        #     pygame.draw.circle(screen, (0, 0, 255), transform(point), 10, 2)
+
+        # for point in pointsLender:
+        #     pygame.draw.circle(screen, (0, 255, 0), transform(point), 10, 2)
 
         pygame.draw.lines(screen, (10,10,10), True, transformList(result),5)
+        # pygame.draw.lines(screen, (0,0,255), True, transformList(pointsHuby),5)
+        # pygame.draw.lines(screen, (0,255,0), True, transformList(pointsLender),5)
 
         pygame.display.flip()
 
